1f0cd3e46c239512874794f32df35c6460d7a1d8
Luis Caballeros <Luis Caballeros@LuisCaballeros>
2018-03-10 23:09:05 -0600

    estoy trabajando en mi tarea

--- src/ipc1/tarea4_201504204/IPC1Tarea4_201504204.java
+++ src/ipc1/tarea4_201504204/IPC1Tarea4_201504204.java
@@ -29,6 +29,51 @@ public class IPC1Tarea4_201504204 implements ActionListener {
         vent.Ventana();
     }
 
-    
+    public void Ventana() {
+
+        ventana.setTitle("Calculadora");
+        ventana.setSize(400, 300);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(ventana.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+        ventana.setLayout(null);
+
+        texto1.setBounds(30, 20, 100, 40);
+        ventana.add(texto1);
+
+        texto2.setBounds(140, 20, 100, 40);
+        ventana.add(texto2);
+
+        texto3.setBounds(250, 20, 100, 40);
+        ventana.add(texto3);
+
+        botonmas.setBounds(50, 90, 75, 50);
+        botonmas.setText("+");
+        ventana.add(botonmas);
+
+        botonmenos.setBounds(150, 90, 75, 50);
+        botonmenos.setText("-");
+        ventana.add(botonmenos);
+
+        botonpor.setBounds(50, 170, 75, 50);
+        botonpor.setText("*");
+        ventana.add(botonpor);
+
+        botondiv.setBounds(150, 170, 75, 50);
+        botondiv.setText("/");
+        ventana.add(botondiv);
+
+        botonigual.setBounds(260, 130, 75, 50);
+        botonigual.setText("=");
+        ventana.add(botonigual);
+
+        //accion click
+        botonmas.addActionListener(this);
+        botonmenos.addActionListener(this);
+        botonpor.addActionListener(this);
+        botondiv.addActionListener(this);
+        botonigual.addActionListener(this);
+
+    }
 
 }
